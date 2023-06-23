e81ff735b594ee50fafef56acf165c3ddd9f1d00
Tang Wuguo <tangwg@csu.edu.cn>
2023-06-23 18:34:57 +0800

    自定义SupEngine1.0

--- code/run.py
+++ code/run.py
@@ -1,11 +1,18 @@
-from torchdrug import datasets, core, tasks, transforms, models, layers
+from torchdrug import core, data, transforms, models, layers, utils
 from torchdrug.layers import geometry
-import torch
+from torchdrug.utils import comm
 import os
+import torch
+from torch import distributed as dist
+from torch import nn
+from torch.utils import data as torch_data
+from itertools import islice
+
 from ScopeSuperFamilyClassify import ScopeSuperFamilyClassify
 from SuperFamilyTask import SuperfamilyPrediction
+from SupEngine import SupEngine
 
-os.environ["CUDA_VISIBLE_DEVICES"] = "0,1"
+os.environ["CUDA_VISIBLE_DEVICES"] = "4,5"
 
 db_path = '/home/tangwuguo/datasets/scope40_s'
 db_file = 'scop40_s.csv'  # 'pdb_scope_db40.csv'
@@ -19,9 +26,7 @@ dataset = ScopeSuperFamilyClassify(path= db_path,
                                     dataset_file=db_file,
                                     pdb_dir=pdb_dir,
                                     transform=transform, verbose=1)
-
 train_set, valid_set, test_set = dataset.split()
-print("Shape of function labels for a protein: ", dataset[0]["superfamily_label"].shape)
 print("train samples: %d, valid samples: %d, test samples: %d" % (len(train_set), len(valid_set), len(test_set)))
 
 # GearNet model
@@ -40,9 +45,8 @@ task = SuperfamilyPrediction(gearnet, num_mlp_layer=3,
                             num_class=2065, verbose=1)
 
 optimizer = torch.optim.Adam(task.parameters(), lr=1e-4)
-solver = core.Engine(task, train_set, valid_set, test_set, optimizer,
-                     gpus=[0], batch_size=32)
-
-solver.train(num_epoch=5)
+solver = SupEngine(task, train_set, valid_set, test_set, optimizer,
+                     gpus=[4], batch_size=64)
+solver.train(num_epoch=10)
 solver.evaluate("valid")
 solver.evaluate("test")
